61f4e148c9323b224346083e72b03aba970cdb75
Patrick Lower <devvortex@gmail.com>
2012-04-17 11:41:34 -0400

    fixed up links and prebuilt

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,12 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/gummy_toro.mk \
-    $(LOCAL_DIR)/gummy_maguro.mk \
-    $(LOCAL_DIR)/gummy_wingray.mk \
-    $(LOCAL_DIR)/gummy_crespo4g.mk \
-    $(LOCAL_DIR)/gummy_fascinatemtd.mk \
-    $(LOCAL_DIR)/gummy_vibrantmtd.mk \
-    $(LOCAL_DIR)/gummy_captivatemtd.mk \
-    $(LOCAL_DIR)/gummy_mesmerizemtd.mk \
-    $(LOCAL_DIR)/gummy_showcasemtd.mk \
-    $(LOCAL_DIR)/gummy_galaxysmtd.mk \
-    $(LOCAL_DIR)/gummy_crespo.mk 
+    $(LOCAL_DIR)/sourcery_toro.mk
--- products/common.mk
+++ products/common.mk
@@ -1,18 +1,16 @@
-PRODUCT_NAME := Gummy
-PODUCT_BRAND := Gummy
+PRODUCT_NAME := Sourcery
+PODUCT_BRAND := Sourcery
 PRODUCT_DEVICE := generic
 
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.rommanager.developerid=Kejar31
+    ro.rommanager.developerid=DeVorteX
 
-# stadard gummy packeages 
+# stadard sourcery packages 
 PRODUCT_PACKAGES := \
     Camera \
     Gallery \
     SoundRecorder \
-    FileManager \
-    LibertySettings \
     AppWidgetPicker \
     Superuser \
     Superuser.apk \
@@ -25,11 +23,9 @@ PRODUCT_PACKAGES += \
     LiveWallpapersPicker \
     VisualizationWallpapers \
     PhaseBeam \
-    GummyBeam \
     HoloSpiralWallpaper \
     NoiseField \
     MagicSmokeWallpapers \
-    Gummypapers \
     Galaxy4 
 
 #CM apps - Thanks to the great team over at CyanogenMod we have these awesome apps :)
@@ -40,89 +36,86 @@ PRODUCT_PACKAGES += \
 
 # init.d support
 PRODUCT_COPY_FILES += \
-    vendor/Gummy/prebuilt/common/etc/init.d/00check:system/etc/init.d/00check \
-    vendor/Gummy/prebuilt/common/etc/init.d/01zipalign:system/etc/init.d/01zipalign \
-    vendor/Gummy/prebuilt/common/etc/init.d/02sysctl:system/etc/init.d/02sysctl \
-    vendor/Gummy/prebuilt/common/etc/init.d/03firstboot:system/etc/init.d/03firstboot \
-    vendor/Gummy/prebuilt/common/etc/init.d/05freemem:system/etc/init.d/05freemem \
-    vendor/Gummy/prebuilt/common/etc/init.d/06removecache:system/etc/init.d/06removecache \
-    vendor/Gummy/prebuilt/common/etc/init.d/07fixperms:system/etc/init.d/07fixperms \
-    vendor/Gummy/prebuilt/common/etc/init.d/09cron:system/etc/init.d/09cron \
-    vendor/Gummy/prebuilt/common/etc/init.d/10sdboost:system/etc/init.d/10sdboost \
-    vendor/Gummy/prebuilt/common/etc/init.d/98tweaks:system/etc/init.d/98tweaks \
-    vendor/Gummy/prebuilt/common/etc/init_trigger.enabled:system/etc/init_trigger.enabled \
-    vendor/Gummy/prebuilt/common/etc/liberty.bsh:system/etc/liberty.bsh \
-    vendor/Gummy/prebuilt/common/etc/sysctl.conf:system/etc/sysctl.conf \
-    vendor/Gummy/prebuilt/common/bin/sysinit:system/bin/sysinit \
+    vendor/Sourcery/prebuilt/common/etc/init.d/00check:system/etc/init.d/00check \
+    vendor/Sourcery/prebuilt/common/etc/init.d/01zipalign:system/etc/init.d/01zipalign \
+    vendor/Sourcery/prebuilt/common/etc/init.d/02sysctl:system/etc/init.d/02sysctl \
+    vendor/Sourcery/prebuilt/common/etc/init.d/03firstboot:system/etc/init.d/03firstboot \
+    vendor/Sourcery/prebuilt/common/etc/init.d/05freemem:system/etc/init.d/05freemem \
+    vendor/Sourcery/prebuilt/common/etc/init.d/06removecache:system/etc/init.d/06removecache \
+    vendor/Sourcery/prebuilt/common/etc/init.d/07fixperms:system/etc/init.d/07fixperms \
+    vendor/Sourcery/prebuilt/common/etc/init.d/09cron:system/etc/init.d/09cron \
+    vendor/Sourcery/prebuilt/common/etc/init.d/10sdboost:system/etc/init.d/10sdboost \
+    vendor/Sourcery/prebuilt/common/etc/init.d/98tweaks:system/etc/init.d/98tweaks \
+    vendor/Sourcery/prebuilt/common/etc/init_trigger.enabled:system/etc/init_trigger.enabled \
+    vendor/Sourcery/prebuilt/common/etc/liberty.bsh:system/etc/liberty.bsh \
+    vendor/Sourcery/prebuilt/common/etc/sysctl.conf:system/etc/sysctl.conf \
+    vendor/Sourcery/prebuilt/common/bin/sysinit:system/bin/sysinit \
 
 	# Propiortary applications
 PRODUCT_COPY_FILES += \
-    vendor/Gummy/prebuilt/common/app/ApexLauncher.apk:system/app/ApexLauncher.apk \
-    vendor/Gummy/prebuilt/common/app/RomToolbox.apk:system/app/RomToolbox.apk \
-    vendor/Gummy/prebuilt/common/app/RootBrowser.apk:system/app/RootBrowser.apk \
-    vendor/Gummy/prebuilt/common/etc/.root_browser:system/etc/.root_browser
+    vendor/Sourcery/prebuilt/common/app/ApexLauncher.apk:system/app/ApexLauncher.apk
 
 # Google DNS servers and host file
 PRODUCT_COPY_FILES += \
-    vendor/Gummy/prebuilt/common/etc/resolv.conf:system/etc/resolv.conf \
-    vendor/Gummy/prebuilt/common/etc/hosts:system/etc/hosts
+    vendor/Sourcery/prebuilt/common/etc/resolv.conf:system/etc/resolv.conf \
+    vendor/Sourcery/prebuilt/common/etc/hosts:system/etc/hosts
 
 # Cron schedual 
 PRODUCT_COPY_FILES += \
-    vendor/Gummy/prebuilt/common/etc/cron/cron.conf:system/etc/cron/cron.conf \
-    vendor/Gummy/prebuilt/common/etc/cron/cron.hourly/00drop_caches:system/etc/cron/cron.hourly/00drop_caches \
-    vendor/Gummy/prebuilt/common/etc/cron/cron.daily/00drop_caches:system/etc/cron/cron.daily/00drop_caches \
-    vendor/Gummy/prebuilt/common/etc/cron/cron.weekly/00drop_caches:system/etc/cron/cron.weekly/00drop_caches \
-    vendor/Gummy/prebuilt/common/etc/cron/cron.hourly/01clear_cache:system/etc/cron/cron.hourly/01clear_cache \
-    vendor/Gummy/prebuilt/common/etc/cron/cron.daily/01clear_cache:system/etc/cron/cron.daily/01clear_cache \
-    vendor/Gummy/prebuilt/common/etc/cron/cron.weekly/01clear_cache:system/etc/cron/cron.weekly/01clear_cache 
+    vendor/Sourcery/prebuilt/common/etc/cron/cron.conf:system/etc/cron/cron.conf \
+    vendor/Sourcery/prebuilt/common/etc/cron/cron.hourly/00drop_caches:system/etc/cron/cron.hourly/00drop_caches \
+    vendor/Sourcery/prebuilt/common/etc/cron/cron.daily/00drop_caches:system/etc/cron/cron.daily/00drop_caches \
+    vendor/Sourcery/prebuilt/common/etc/cron/cron.weekly/00drop_caches:system/etc/cron/cron.weekly/00drop_caches \
+    vendor/Sourcery/prebuilt/common/etc/cron/cron.hourly/01clear_cache:system/etc/cron/cron.hourly/01clear_cache \
+    vendor/Sourcery/prebuilt/common/etc/cron/cron.daily/01clear_cache:system/etc/cron/cron.daily/01clear_cache \
+    vendor/Sourcery/prebuilt/common/etc/cron/cron.weekly/01clear_cache:system/etc/cron/cron.weekly/01clear_cache 
 
 # Compcache/Zram support
 PRODUCT_COPY_FILES += \
-    vendor/Gummy/prebuilt/common/etc/init.local.rc:system/etc/init.local.rc \
-    vendor/Gummy/prebuilt/common/bin/compcache:system/bin/compcache \
-    vendor/Gummy/prebuilt/common/bin/handle_compcache:system/bin/handle_compcache
+    vendor/Sourcery/prebuilt/common/etc/init.local.rc:system/etc/init.local.rc \
+    vendor/Sourcery/prebuilt/common/bin/compcache:system/bin/compcache \
+    vendor/Sourcery/prebuilt/common/bin/handle_compcache:system/bin/handle_compcache
 
 # Term binarry for support for market updates
 PRODUCT_COPY_FILES +=  \
-    vendor/Gummy/proprietary/Term.apk:system/app/Term.apk \
-    vendor/Gummy/proprietary/lib/armeabi/libjackpal-androidterm3.so:system/lib/libjackpal-androidterm3.so 
+    vendor/Sourcery/proprietary/Term.apk:system/app/Term.apk \
+    vendor/Sourcery/proprietary/lib/armeabi/libjackpal-androidterm3.so:system/lib/libjackpal-androidterm3.so 
 
 # Term info for nano support
 PRODUCT_COPY_FILES += \
-    vendor/Gummy/prebuilt/common/etc/terminfo/l/linux:system/etc/terminfo/l/linux \
-    vendor/Gummy/prebuilt/common/etc/terminfo/u/unknown:system/etc/terminfo/u/unknown
+    vendor/Sourcery/prebuilt/common/etc/terminfo/l/linux:system/etc/terminfo/l/linux \
+    vendor/Sourcery/prebuilt/common/etc/terminfo/u/unknown:system/etc/terminfo/u/unknown
 
 # Adding files needed for extra camera features
 PRODUCT_COPY_FILES += \
-    vendor/Gummy/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
-    vendor/Gummy/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd \
-    vendor/Gummy/prebuilt/common/media/video/AndroidInSpace.240p.mp4:system/media/video/AndroidInSpace.240p.mp4 \
-    vendor/Gummy/prebuilt/common/media/video/AndroidInSpace.480p.mp4:system/media/video/AndroidInSpace.480p.mp4 \
-    vendor/Gummy/prebuilt/common/media/video/Disco.240p.mp4:system/media/video/Disco.240p.mp4 \
-    vendor/Gummy/prebuilt/common/media/video/Disco.480p.mp4:system/media/video/Disco.480p.mp4 \
-    vendor/Gummy/prebuilt/common/media/video/Sunset.240p.mp4:system/media/video/Sunset.240p.mp4 \
-    vendor/Gummy/prebuilt/common/media/video/Sunset.480p.mp4:system/media/video/Sunset.480p.mp4 
+    vendor/Sourcery/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
+    vendor/Sourcery/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd \
+    vendor/Sourcery/prebuilt/common/media/video/AndroidInSpace.240p.mp4:system/media/video/AndroidInSpace.240p.mp4 \
+    vendor/Sourcery/prebuilt/common/media/video/AndroidInSpace.480p.mp4:system/media/video/AndroidInSpace.480p.mp4 \
+    vendor/Sourcery/prebuilt/common/media/video/Disco.240p.mp4:system/media/video/Disco.240p.mp4 \
+    vendor/Sourcery/prebuilt/common/media/video/Disco.480p.mp4:system/media/video/Disco.480p.mp4 \
+    vendor/Sourcery/prebuilt/common/media/video/Sunset.240p.mp4:system/media/video/Sunset.240p.mp4 \
+    vendor/Sourcery/prebuilt/common/media/video/Sunset.480p.mp4:system/media/video/Sunset.480p.mp4 
 
 # Added xbin files
 PRODUCT_COPY_FILES += \
-    vendor/Gummy/prebuilt/common/xbin/backup:system/xbin/backup \
-    vendor/Gummy/prebuilt/common/xbin/bash:system/xbin/bash \
-    vendor/Gummy/prebuilt/common/xbin/btool:system/xbin/btool \
-    vendor/Gummy/prebuilt/common/xbin/busybox:system/xbin/busybox \
-    vendor/Gummy/prebuilt/common/xbin/drm1_func_test:system/xbin/drm1_func_test \
-    vendor/Gummy/prebuilt/common/xbin/gdb:system/xbin/gdb \
-    vendor/Gummy/prebuilt/common/xbin/gdbserver:system/xbin/gdbserver \
-    vendor/Gummy/prebuilt/common/xbin/market_history:system/xbin/market_history \
-    vendor/Gummy/prebuilt/common/xbin/nano:system/xbin/nano \
-    vendor/Gummy/prebuilt/common/xbin/rm_apks:system/xbin/rm_apks \
-    vendor/Gummy/prebuilt/common/xbin/run_backup:system/xbin/run_backup \
-    vendor/Gummy/prebuilt/common/xbin/run_restore:system/xbin/run_restore \
-    vendor/Gummy/prebuilt/common/xbin/ssmgrd:system/xbin/ssmgrd \
-    vendor/Gummy/prebuilt/common/xbin/sysro:system/xbin/sysro \
-    vendor/Gummy/prebuilt/common/xbin/sysrw:system/xbin/sysrw \
-    vendor/Gummy/prebuilt/common/xbin/zip:system/xbin/zip \
-    vendor/Gummy/prebuilt/common/xbin/zipalign:system/xbin/zipalign 
+    vendor/Sourcery/prebuilt/common/xbin/backup:system/xbin/backup \
+    vendor/Sourcery/prebuilt/common/xbin/bash:system/xbin/bash \
+    vendor/Sourcery/prebuilt/common/xbin/btool:system/xbin/btool \
+    vendor/Sourcery/prebuilt/common/xbin/busybox:system/xbin/busybox \
+    vendor/Sourcery/prebuilt/common/xbin/drm1_func_test:system/xbin/drm1_func_test \
+    vendor/Sourcery/prebuilt/common/xbin/gdb:system/xbin/gdb \
+    vendor/Sourcery/prebuilt/common/xbin/gdbserver:system/xbin/gdbserver \
+    vendor/Sourcery/prebuilt/common/xbin/market_history:system/xbin/market_history \
+    vendor/Sourcery/prebuilt/common/xbin/nano:system/xbin/nano \
+    vendor/Sourcery/prebuilt/common/xbin/rm_apks:system/xbin/rm_apks \
+    vendor/Sourcery/prebuilt/common/xbin/run_backup:system/xbin/run_backup \
+    vendor/Sourcery/prebuilt/common/xbin/run_restore:system/xbin/run_restore \
+    vendor/Sourcery/prebuilt/common/xbin/ssmgrd:system/xbin/ssmgrd \
+    vendor/Sourcery/prebuilt/common/xbin/sysro:system/xbin/sysro \
+    vendor/Sourcery/prebuilt/common/xbin/sysrw:system/xbin/sysrw \
+    vendor/Sourcery/prebuilt/common/xbin/zip:system/xbin/zip \
+    vendor/Sourcery/prebuilt/common/xbin/zipalign:system/xbin/zipalign 
 
 
 # Enable SIP+VoIP on all targets

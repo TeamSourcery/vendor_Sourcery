7348754d6faf3cb9d71d906d1eb6540a47304b33
Patrick Lower <devvortex@gmail.com>
2012-04-17 00:04:38 -0400

    first commit

---
+++ products/AndroidProducts.mk
@@ -0,0 +1,12 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/gummy_toro.mk \
+    $(LOCAL_DIR)/gummy_maguro.mk \
+    $(LOCAL_DIR)/gummy_wingray.mk \
+    $(LOCAL_DIR)/gummy_crespo4g.mk \
+    $(LOCAL_DIR)/gummy_fascinatemtd.mk \
+    $(LOCAL_DIR)/gummy_vibrantmtd.mk \
+    $(LOCAL_DIR)/gummy_captivatemtd.mk \
+    $(LOCAL_DIR)/gummy_mesmerizemtd.mk \
+    $(LOCAL_DIR)/gummy_showcasemtd.mk \
+    $(LOCAL_DIR)/gummy_galaxysmtd.mk \
+    $(LOCAL_DIR)/gummy_crespo.mk 

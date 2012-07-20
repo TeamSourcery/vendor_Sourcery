fd1183667c3dbdc1692d57eed55f2d5108cca4a0
Eagle1967 <icsourcery@gmail.com>
2012-07-20 12:09:52 -0600

    fix direction path for sip/voip as iots been moved to framework/native

--- products/common.mk
+++ products/common.mk
@@ -139,7 +139,7 @@ PRODUCT_COPY_FILES += \
 
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
-    frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
 
 # Camera shutter sound property
 PRODUCT_PROPERTY_OVERRIDES += \

0ee60c4e7e01a96dd5b3a2de3dd6d849aae6b85d
Eagle1967 <icsourcery@gmail.com>
2012-07-25 18:40:32 -0600

    fix my lack of /

--- products/common.mk
+++ products/common.mk
@@ -66,7 +66,7 @@ PRODUCT_COPY_FILES += \
 # Sounds and bootani
 PRODUCT_COPY_FILES += \
     vendor/Sourcery/prebuilt/common/media/boot_audio.mp3:system/media/boot_audio.mp3 \
-    vendor/Sourcery/prebuilt/common/media/audio/notifications/Sourcery.ogg:system/media/audio/notifications/Sourcery.ogg
+    vendor/Sourcery/prebuilt/common/media/audio/notifications/Sourcery.ogg:system/media/audio/notifications/Sourcery.ogg \
     vendor/Sourcery/prebuilt/toro/bootanimation.zip:system/media/bootanimation.zip
 # libs
 PRODUCT_COPY_FILES += \

813b3e5f1cef64e6b20d23e77fd8a2a875f76567
Eagle1967 <icsourcery@gmail.com>
2012-07-29 17:49:04 -0600

    whoops

--- products/gsm.mk
+++ products/gsm.mk
@@ -4,7 +4,7 @@ PRODUCT_COPY_FILES += \
 
 # GSM SPN overrides list
 PRODUCT_COPY_FILES += \
-    vendor/cm/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
+    vendor/Sourcery/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
 
 
 # SIM Toolkit

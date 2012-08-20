0c3c674f7e924a22e4f7319acbc4876623bcd045
Eagle1967 <icsourcery@gmail.com>
2012-08-19 22:38:35 -0600

    update boot animation, update for release

--- products/common.mk
+++ products/common.mk
@@ -162,7 +162,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=freebird.ogg \
     ro.config.notification_sound=Sourcery.ogg \
     ro.config.alarm_alert=Plutonium.ogg \
-    ro.build.sourcery.version=1.1 \
+    ro.build.sourcery.version=2.0 \
     ro.com.android.dataroaming=false \
 	ro.goo.version=100
 

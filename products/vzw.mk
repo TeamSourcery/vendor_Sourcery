1fe2d7a2a6f102deb4567979a5eaba18f5d1a6d9
eagle1967 <icsourcery@gmail.com>
2012-10-20 01:05:13 -0600

    redirect to correct file location

--- products/vzw.mk
+++ products/vzw.mk
@@ -1,6 +1,6 @@
 #Permissions and Lib for Verizon Wireless Apps
 PRODUCT_COPY_FILES += \
-    vendor/Sourcery/prebuilt/vzw/common/app/VerizonSSO.apk:system/app/VerizonSSO.apk \
+    vendor/Sourcery/prebuilt/common/app/VerizonSSO.apk:system/app/VerizonSSO.apk \
     vendor/Sourcery/vzw/com.vzw.hardware.ehrpd.xml:system/etc/permissions/com.vzw.hardware.ehrpd.xml \
     vendor/Sourcery/vzw/com.vzw.hardware.lte.xml:system/etc/permissions/com.vzw.hardware.lte.xml \
     vendor/Sourcery/vzw/com.vzw.vzwapnlib.xml:system/etc/permissions/com.vzw.vzwapnlib.xml \

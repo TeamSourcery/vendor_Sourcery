6fc2e66a2f3ac286e99eefd6f9208bc2caa14a22
eagle1967 <icsourcery@gmail.com>
2012-11-09 19:52:22 -0700

    update ofr 4.2 release-update apex and trikstermod

--- products/common.mk
+++ products/common.mk
@@ -161,9 +161,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=freebird.ogg \
     ro.config.notification_sound=Sourcery.ogg \
     ro.config.alarm_alert=Plutonium.ogg \
-    ro.build.sourcery.version=3.1.5 \
+    ro.build.sourcery.version=3.2 \
     ro.com.android.dataroaming=false \
-    ro.goo.version=315 \
+    ro.goo.version=320 \
     ro.goo.rom=JBSourcery
 
 

ee7748a724aa15a0a8cb6f99d3756c4107baf394
eagle1967 <icsourcery@gmail.com>
2013-01-07 15:25:19 -0700

    update to 4.3

--- products/common.mk
+++ products/common.mk
@@ -171,9 +171,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=freebird.ogg \
     ro.config.notification_sound=Sourcery.ogg \
     ro.config.alarm_alert=Plutonium.ogg \
-    ro.build.sourcery.version=4.2 \
+    ro.build.sourcery.version=4.3 \
     ro.com.android.dataroaming=false \
-    ro.goo.version=420 \
+    ro.goo.version=430 \
     ro.goo.rom=JBSourcery
 
 
